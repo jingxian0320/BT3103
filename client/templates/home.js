70c8d89937a66a4f68febe78fdba13a8f9d83fdf
jingxian0320 <baojingxian0320@gmail.com>
2016-10-06 11:15:29 +0800

    create order

--- client/templates/home.js
+++ client/templates/home.js
@@ -35,4 +35,14 @@ Template.home.events({
       return Overlay.open('authOverlay');
 
   },
+  'submit'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+    // Get value from form element
+    var target = event.target;
+    var table_id = target.tableId.value;
+    if (! Meteor.userId())
+      return Overlay.open('authOverlay');
+    Meteor.call('createOrder', table_id);
+  }
 });
