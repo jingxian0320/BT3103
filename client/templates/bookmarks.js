f9dc3f5d64e4f19a77bdfe2c31e6b88bc80bbdba
shenlinnn <slinda1992@gmail.com>
2016-10-21 12:51:24 +0800

    UI for cart

--- client/templates/bookmarks.js
+++ client/templates/bookmarks.js
@@ -1,5 +1,17 @@
-Template.bookmarks.helpers({
-  recipeCount: function() {
-    return pluralize(this.length, 'recipe');
+Template.bookmarks.events({
+  'click .delete': function(){
+    Meteor.call('deleteDish', this.name);
+  },
+
+  'click .plus': function(){
+    Meteor.call('addQuantity',this.name);
+  },
+
+  'click .minus': function(){
+    if (this.qty < 2) {
+      $('.minus').disabled = true;
+    } else{
+      Meteor.call('subtractQuantity',this.name);
+    }
   }
-});
+})
