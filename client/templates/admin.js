eee3c49eca6a1bd3bd9f65f79d2e31a112ba3e1c
jingxian0320 <baojingxian0320@gmail.com>
2016-10-31 02:04:43 +0800

    admin chart

--- client/templates/admin.js
+++ client/templates/admin.js
@@ -1,8 +1,13 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+var Highcharts = require('highcharts');
+require('highcharts/modules/exporting')(Highcharts);
+
 Template.admin.helpers({
   isAdmin: function() {
     return Meteor.user() && Meteor.user().admin;
   },
-  
+
   latestNews: function() {
     return News.latest();
   }
@@ -17,8 +22,94 @@ Template.admin.events({
 
     alert('Saved latest news');
   },
-  
+
   'click .login': function() {
     Meteor.loginWithTwitter();
   }
 })
+
+
+var options = {
+    chart: {
+        renderTo: 'container',
+        defaultSeriesType: 'bar',
+    },
+
+    title: {
+      text: 'Top 5 Most Popular Dishes'
+    },
+    subtitle: {
+      text: 'Source: Order-details dataset'
+    },
+    xAxis: {
+      categories: ['Cheese Fries', 'Seafood Fried Rice', 'Cheese Cake', 'Pineapple Fried Rice', 'Lemon Tea'],
+      title: {
+        text: 'Dishes'
+      }
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: 'Average Daily Orders',
+        align: 'high'
+      },
+      labels: {
+        overflow: 'justify'
+      }
+    },
+    plotOptions: {
+      bar: {
+        dataLabels: {
+          enabled: true
+        }
+      }
+    },
+    legend: {
+      enabled: false
+    },
+    credits: {
+      enabled: false
+    },
+    series: [{name: 'Orders', data: [50, 35, 34, 20, 12]}]
+};
+
+
+Template.chart.helpers({
+    createChart: function () {
+      Meteor.defer(function() {
+        Highcharts.chart('chartplace',options);
+      })
+    },
+    remarks: function () {
+      if (Session.get('option') === 'shared'){
+        return 'Include only orders shared by two or more customers'
+      }
+      else{
+        return 'Include all orders'
+      }
+
+    }
+  })
+
+Template.chart.events({
+  'change #list': function (event) {
+    //alert('f')
+
+    var selVal = event.target.value;
+    Session.set('option',selVal)
+    if(selVal == "all" || selVal == '')
+    {
+    		options.title.text = 'Top 5 Most Popular Dishes';
+        options.xAxis.categories = ['Cheese Fries', 'Seafood Fried Rice', 'Cheese Cake', 'Pineapple Fried Rice', 'Lemon Tea']
+        options.series = [{name: 'Orders', data: [50, 35, 34, 20, 12]}]
+    }
+    else
+    {
+    		options.title.text = 'Top 5 Most Popular Shared Dishes';
+        options.xAxis.categories = ['Cheese Fries', 'Cheese Cake', 'Seafood Fried Rice', 'Pineapple Fried Rice', 'Lemon Tea'];
+        options.series = [{name: 'Orders', data: [32, 25, 14, 5, 2]}];
+
+    }
+    Highcharts.chart('chartplace',options);
+  }
+});
